09d90f943909c17070922edcf9452f70811c5aaa
Carter Klein <carterklein13@gmail.com>
2020-01-18 15:06:53 -0500

    Added generate all palindromes, and a general README

---
+++ GenerateAllPalindromes.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class GenerateAllPalindromes { 
+    public static void generateAllPalindromes(String s) {
+        if (s == null || s.length() < 1) System.out.println("Invalid input");
+        
+        for (int i = 0; i < s.length(); i++) {
+            expand(s, i, i);
+            if (i+1 < s.length() && s.charAt(i) == s.charAt(i+1)) {
+                expand(s, i, i+1);
+            }
+        }
+    }
+
+    private static void expand(String s, int left, int right) {
+        if (s == null || left > right) System.out.println("Invalid string");
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+            System.out.print("Palindrome: " + s.substring(left+1, right) + " | ");
+        }
+        return;
+    }
+
+    public static void main(String[] args) {
+        List<String> testSuite = new ArrayList<>();
+        testSuite.add("aaabbaaa");
+        testSuite.add("racecar");
+        testSuite.add("abcddefg");
+        testSuite.add("");
+        testSuite.add("abcdefg");
+        
+        for (int i = 0; i < testSuite.size(); i++) {
+            System.out.println("Test " + i + ": " + testSuite.get(i));
+            generateAllPalindromes(testSuite.get(i));
+            System.out.println();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# General Tips, Tricks, Definitions:
+
+## Definitions:
+* **Pass by reference**: you pass the actual memory reference of the object into the following function. Whatever you change in that function changes the actual value of the object. If you pass a CoffeeCup object into a fill(CoffeeCup) method from main(), the CoffeeCup object in main will be filled up
+* **Pass by value**: you pass in a copy of the object into the following function. If you pass a CoffeeCup object into a fill(CoffeeCup) method from main(), the CoffeeCup object in main is not filled up.
+
+## General Tips & Tricks:
+* If you want to pass an int (**or any primitive type**) by reference in Java, you inherently cannot. What you *can* do, however, is create a 1-element array to hold the value of that primitive, and pass that by reference
+
+```
+public static void main(String[] args) {
+    int[] count = new int[]{0};
+    for (int i = 0; i < 10; i++) {
+        tick(count);
+    }
+}
+
+private void tick(int[] count) {
+    count[0]++;
+}
+```
