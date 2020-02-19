6e96037ce99e03beb0186afae991730e15451b53
Carter Klein <carterklein13@gmail.com>
2020-02-18 23:37:10 -0500

    Added another List problemo I'm a whiz tonight let's get it

---
+++ RemoveNthFromEndOfList.java
@@ -0,0 +1,51 @@
+import Common.ListNode;
+
+class RemoveNthFromEnd {
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        // Edge case
+        if (head == null) return null;
+
+        // Get the length of the list
+        int length = 0;
+        ListNode curr = head;
+        while (curr != null) {
+            length++;
+            curr = curr.next;
+        }
+
+        // A couple more edge cases pop up here
+        if (length == 1) return null;
+        if (length == n) return head.next;
+        
+        // Remove specified node
+        curr = head;
+        int count = 1;
+        while (count < length-n) {
+            curr = curr.next;
+            count++;
+        }
+        curr.next = curr.next.next;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
+        l1.next.next.next = new ListNode(4);
+        l1.next.next.next.next = new ListNode(5);
+
+        ListNode curr = l1;
+        while (curr != null) {
+            System.out.print(curr.val+"->");
+            curr = curr.next;
+        }
+        System.out.println("->null\nRemove 3");
+        curr = removeNthFromEnd(l1, 3);
+        while (curr != null) {
+            System.out.print(curr.val+"->");
+            curr = curr.next;
+        }
+        System.out.println("->null");
+    }
+}
